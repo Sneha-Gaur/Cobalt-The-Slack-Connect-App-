6c39ebabe144d5da9d76344b87f2b2dc4524d059
sneha gaur <snehagaur1911@gmail.com>
2025-08-10 19:50:51 +0530

    Create Message.ts

---
+++ backend/models /Message.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  channel: { type: String, required: true },
+  message: { type: String, required: true },
+  send_at: { type: Date, required: true },  
+  team_id: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ['scheduled', 'sent', 'cancelled'],
+    default: 'scheduled'
+  }
+}, { timestamps: true });
+
+export const MessageModel = mongoose.model('Message', messageSchema);
